efafb2e686673c2052e81f2101bdcdc4cf1cf1ab
Owen-E22 <owen.eigen@icloud.com>
2024-11-01 16:52:14 -0500

    week 9

--- program_1.py
+++ program_1.py
@@ -3,11 +3,30 @@
 # (Use the included example file names.txt) and exists on the computer's disk.
 # Write a program that displays the number of names that are stored in the file.
 
+
 def count_file_lines():
+    names = open('names.txt', 'w')
+    names.write('Owen\n')
+    names.write('John\n')
+    names.write('Sally\n')
+    names.write('Tom\n')
+    names.write('Dave\n')
+    names.write('Beth\n')
+    names.close()
+
+    infile = open('names.txt', 'r')
+    total = 0
+    name = infile.readline()
+    name = name.rstrip('\n')
+
+
+    while name != '':
+        name = infile.readline()
+        name = name.rstrip('\n')
+        total += 1
+
     ######################
-    # Add your code here #
-    ######################
-    print('In the count_file_lines function')
+    print(f'There are {total} names in the file')
 
 
 
